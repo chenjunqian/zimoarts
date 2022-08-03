e59b891d002467ed6f90c88ea95c9752b2aebe7a
chenjunqian <chenjunqian0810@foxmail.com>
2022-08-04 00:34:15 +0800

    new footer

--- zimoarts/src/components/FootComponent.vue
+++ zimoarts/src/components/FootComponent.vue
@@ -1,37 +1,34 @@
 <template>
   <div class="footer-container">
-    <div class="left-footer-container">
-      <div class="left-footer-top-text">Groton School</div>
-      <div class="left-footer-bottom-text">
-        @2022 GROTON SCHOOL ALL RIGHTS RESERVED
+    <div class="footer-container-left-placeholder"></div>
+    <div class="footer-container-center-wrapper">
+      <div class="left-footer-container">
+        <div class="left-footer-top-text">Groton School</div>
+        <div class="left-footer-text-bottom-container">
+          <div class="left-footer-bottom-text">
+            @2022 GROTON SCHOOL ALL RIGHTS RESERVED
+          </div>
+        </div>
+      </div>
+      <div class="center-footer-container">
+        <img
+          class="center-footer-logo"
+          src="@/assets/school-bagde.png"
+          alt=""
+        />
+      </div>
+      <div class="right-footer-container">
+        <div class="right-footer-logo">GROTONARTS</div>
+        <div class="right-footer-text-column">
+          <div class="right-footer-text">JGYTDETRTQWEESDFG</div>
+          <div class="right-footer-text">GDGHGHGDFGSDTSIUHIOVBBXZMIYTUXDG</div>
+          <div class="right-footer-text">HJHFJH JHCH V FJ VJVCCCC H</div>
+          <div class="right-footer-text">GJHG JKHVJHG VHVHFV</div>
+          <div class="right-footer-text">CHGFD</div>
+        </div>
       </div>
     </div>
-    <div class="center-footer-container">
-      <div class="center-footer-text-column">
-        <div class="center-footer-text">JGYTDETRTQWEESDFG</div>
-        <div class="center-footer-text">GDGHGHGDFGSDTSIUHIOVBBXZMIYTUXDG</div>
-        <div class="center-footer-text">HJHFJH JHCH V FJ VJVCCCC H</div>
-        <div class="center-footer-text">GJHG JKHVJHG VHVHFV</div>
-        <div class="center-footer-text">CHGFD</div>
-      </div>
-      <div class="center-footer-text-column">
-        <div class="center-footer-text">JGYTDETRTQWEESDFG</div>
-        <div class="center-footer-text">GDGHGHGDFGSDTSIUHIOVBBXZMIYTUXDG</div>
-        <div class="center-footer-text">HJHFJH JHCH V FJ VJVCCCC H</div>
-        <div class="center-footer-text">GJHG JKHVJHG VHVHFV</div>
-        <div class="center-footer-text">CHGFD</div>
-      </div>
-      <div class="center-footer-text-column">
-        <div class="center-footer-text">JGYTDETRTQWEESDFG</div>
-        <div class="center-footer-text">GDGHGHGDFGSDTSIUHIOVBBXZMIYTUXDG</div>
-        <div class="center-footer-text">HJHFJH JHCH V FJ VJVCCCC H</div>
-        <div class="center-footer-text">GJHG JKHVJHG VHVHFV</div>
-        <div class="center-footer-text">CHGFD</div>
-      </div>
-    </div>
-    <div class="right-footer-container">
-      <div class="foot-logo">Here is logo</div>
-    </div>
+    <div class="footer-container-right-placeholder"></div>
   </div>
 </template>
 
@@ -49,62 +46,92 @@ export default {
   flex-wrap: wrap;
 }
 
+.footer-container-right-placeholder {
+  width: 5%;
+  height: 20px;
+}
+
+.footer-container-left-placeholder {
+  width: 5%;
+  height: 20px;
+}
+
+.footer-container-center-wrapper {
+  width: 90%;
+  border-top: 1px solid #000000;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.top-footer-rights {
+  width: 90%;
+}
+
+/* left block */
 .left-footer-container {
-  width: 20%;
+  width: 30%;
   justify-content: center;
 }
 
 .left-footer-top-text {
   width: 100%;
   height: 30%;
-  padding-top: 15px;
-  padding-left: 20px;
+  margin-top: 15px;
   display: flex;
   font-size: 23px;
-  color: firebrick;
+  color: brown;
   font: bolder;
+}
+
+.left-footer-text-bottom-container {
+  width: 100%;
+  display: flex;
+  height: 50%;
+  position: relative;
 }
 
 .left-footer-bottom-text {
   width: 100%;
-  height: 30%;
-  margin-left: 20px;
-  margin-top: 30px;
   display: flex;
   font-size: 8px;
+  position: absolute;
   bottom: 0;
 }
 
+/* center block */
 .center-footer-container {
-  width: 70%;
+  width: 40%;
   justify-content: center;
   position: relative;
   display: flex;
   flex-wrap: wrap;
 }
 
-.center-footer-text-column {
-  width: 30%;
-  height: 100%;
-  padding-top: 20px;
+.center-footer-logo {
+  width: auto;
+  height: 50px;
+  padding-top: 40px;
 }
 
-.center-footer-text {
-  width: 100%;
-  font-size: 8px;
+/* right block */
+.right-footer-container {
+  width: 30%;
+  justify-content: center;
+}
+
+.right-footer-logo {
+  max-width: 100%;
+  padding-top: 15px;
   text-align: right;
 }
 
-.right-footer-container {
-  width: 10%;
-  justify-content: center;
+.right-footer-text-column {
+  padding-top: 20px;
 }
 
-.foot-logo {
+.right-footer-text {
   width: 100%;
-  height: 60%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  font-size: 8px;
+  text-align: right;
 }
 </style>
