c19dd5866cda5ec434477c38def89c8d6ecaf1f6
chenjunqian <chenjunqian0810@foxmail.com>
2022-08-15 23:37:06 +0800

    add rotate image component

--- zimoarts/src/components/InterviewColumnComponent.vue
+++ zimoarts/src/components/InterviewColumnComponent.vue
@@ -1,6 +1,5 @@
 <template>
-    <div style="width: 100%; background-color: #c0c0c0"
-        :style="{'background-image': `url(${require('@/assets/home-bg-002.png')})`}">
+    <div style="width: 100%; background-color: #c0c0c0" :style="{'background-image': `url('${backgroudPicUrl}')`}">
         <div
             style="width: 100%; padding-top: 50px; margin-bottom: 50px; font-size: 58px; font-weight: bolder; border-bottom: 1px solid #000000; display: flex; justify-content: center; color: #ffffff; ">
             <div>INTERVIEW</div>
@@ -10,23 +9,26 @@
         <div style="width: 100%; border-bottom: 1px solid #000000; padding-top: 50px;
               ">
             <div style="margin-left: 5%; margin-right: 5%; display: flex;color: #ffffff;">
-                <div style="width: 25%; height: 160px; border-left: 1px solid #000000">
-                    <img style=" height: 90%; width: 90%; padding-top: 5%; padding-left: 5%; padding-right: 5%; " />
+                <div style="width: 25%; height: 201.7px; border-left: 1px solid #000000; position: relative;">
+                    <TreeDRotateImg
+                        style=" height: 95%; width: 90%; position: absolute;bottom: 0;right: 5%; left: 5%;"
+                        imageUrl="https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg" />
                 </div>
-                <div style="width: 25%; height: 160px; border-left: 1px solid #000000">
-                    <div style="font-weight: bolder">sadfkjanihgskbcjkooye</div>
+                <div style="width: 25%; height: 201.7px; border-left: 1px solid #000000">
+                    <div style="font-weight: bolder; margin-top: 20px;">sadfkjanihgskbcjkooye</div>
                     <div style=" margin: 0 auto; word-break: break-all; word-wrap: break-word; ">
                         oqnmBslhflafkjsafl alksf;loskfmguugsdqozmxjhzvbn/
                         sadfkjanihgskbcjkooye oqnmBslhflafkjsafl
                         alksf;loskfmguugsdqozmxjhzvbn/
                     </div>
                 </div>
-                <div style="width: 25%; height: 160px; border-left: 1px solid #000000">
-                    <img style=" height: 90%; width: 90%; padding-top: 5%; padding-left: 5%; padding-right: 5%; " />
+                <div style="width: 25%; height: 201.7px; border-left: 1px solid #000000; position: relative;">
+                    <TreeDRotateImg style=" height: 95%; width: 90%; position: absolute;bottom: 0;right: 5%; left: 5%;"
+                        imageUrl="https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg" />
                 </div>
                 <div
-                    style=" width: 25%; height: 160px; border-left: 1px solid #000000; border-right: 1px solid #000000; ">
-                    <div style="font-weight: bolder">sadfkjanihgskbcjkooye</div>
+                    style=" width: 25%; height: 201.7px; border-left: 1px solid #000000; border-right: 1px solid #000000; ">
+                    <div style="font-weight: bolder; margin-top: 20px;">sadfkjanihgskbcjkooye</div>
                     <div style=" margin: 0 auto; word-break: break-all; word-wrap: break-word; ">
                         oqnmBslhflafkjsafl alksf;loskfmguugsdqozmxjhzvbn/
                         sadfkjanihgskbcjkooye oqnmBslhflafkjsafl
@@ -38,28 +40,30 @@
         <!-- bottom colomn -->
         <div style="width: 100%; padding-bottom: 150px;color: #ffffff;">
             <div style="margin-left: 5%; margin-right: 5%; display: flex">
-                <div style="width: 25%; height: 160px; border-left: 1px solid #000000">
-                    <div style="font-weight: bolder">sadfkjanihgskbcjkooye</div>
+                <div style="width: 25%; height: 201.7px; border-left: 1px solid #000000">
+                    <div style="font-weight: bolder; margin-top: 20px;">sadfkjanihgskbcjkooye</div>
                     <div style=" margin: 0 auto; word-break: break-all; word-wrap: break-word; ">
                         oqnmBslhflafkjsafl alksf;loskfmguugsdqozmxjhzvbn/
                         sadfkjanihgskbcjkooye oqnmBslhflafkjsafl
                         alksf;loskfmguugsdqozmxjhzvbn/
                     </div>
                 </div>
-                <div style="width: 25%; height: 160px; border-left: 1px solid #000000">
-                    <img style=" height: 90%; width: 90%; padding-top: 5%; padding-left: 5%; padding-right: 5%; " />
+                <div style="width: 25%; height: 201.7px; border-left: 1px solid #000000;position: relative;">
+                    <TreeDRotateImg style=" height: 95%; width: 90%; position: absolute;top: 0;right: 5%; left: 5%;"
+                        imageUrl="https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg" />
                 </div>
                 <div
-                    style=" width: 25%; height: 160px; border-left: 1px solid #000000; border-right: 1px solid #000000; ">
-                    <div style="font-weight: bolder">sadfkjanihgskbcjkooye</div>
+                    style=" width: 25%; height: 201.7px; border-left: 1px solid #000000; border-right: 1px solid #000000; ">
+                    <div style="font-weight: bolder; margin-top: 20px;">sadfkjanihgskbcjkooye</div>
                     <div style=" margin: 0 auto; word-break: break-all; word-wrap: break-word; ">
                         oqnmBslhflafkjsafl alksf;loskfmguugsdqozmxjhzvbn/
                         sadfkjanihgskbcjkooye oqnmBslhflafkjsafl
                         alksf;loskfmguugsdqozmxjhzvbn/
                     </div>
                 </div>
-                <div style="width: 25%; height: 160px; border-right: 1px solid #000000">
-                    <img style=" height: 90%; width: 90%; padding-top: 5%; padding-left: 5%; padding-right: 5%; " />
+                <div style="width: 25%; height: 201.7px; border-right: 1px solid #000000;position: relative;">
+                    <TreeDRotateImg style=" height: 95%; width: 90%; position: absolute;top: 0;right: 5%; left: 5%;"
+                        imageUrl="https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg" />
                 </div>
             </div>
         </div>
@@ -67,29 +71,26 @@
 </template>
 
 <script>
+    import TreeDRotateImg from './TreeDRotateImg.vue';
     export default {
         name: "InterviewColumnComponent",
         props: {
             columnOneTitle: String,
             columnOneDesc: String,
             columnOneImgUrl: String,
-
             columnTwoTitle: String,
             columnTwoDesc: String,
             columnTwoImgUrl: String,
-
             columnTreeTitle: String,
             columnTreeDesc: String,
             columnTreeImgUrl: String,
-
             columnFourTitle: String,
             columnFourDesc: String,
             columnFourImgUrl: String,
-
-            backgroundImgUrl: String,
-
+            backgroudPicUrl: String,
             titleColor: String
-        }
+        },
+        components: { TreeDRotateImg }
     }
 </script>
 
---
+++ zimoarts/src/components/TreeDRotateImg.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="box">
+        <img class="front" :src="imageUrl" />
+        <img class="back" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TreeDRotateImg",
+        props: {
+            imageUrl: String
+        }
+    }
+</script>
+
+<style>
+    .box {
+        position: relative;
+        width: 300px;
+        height: 300px;
+        cursor: pointer;
+        transform-style: preserve-3d;
+    }
+
+    .box:hover {
+        animation: rotate 5s cubic-bezier(0.5, 0.2, 0.1, 1);
+    }
+
+    .front,
+    .back {
+        position: absolute;
+        bottom: 0;
+        left: 1%;
+        right: 1%;
+        color: #fff;
+        width: 98%;
+        height: 100%;
+        /* 隐藏元素背面 */
+        backface-visibility: hidden;
+    }
+
+    .back {
+        opacity: 0.5;
+        background-color: skyblue;
+        transform: rotateY(180deg);
+    }
+
+    @keyframes rotate {
+        0% {
+            transform: rotateY(0deg);
+        }
+
+        100% {
+            transform: rotateY(360deg);
+        }
+    }
+
+    @keyframes rotate-reverse {
+        0% {
+            transform: rotateY(360deg);
+        }
+
+        100% {
+            transform: rotateY(0deg);
+        }
+    }
+</style>
