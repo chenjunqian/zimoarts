4a35559a19d9feaeeaf9338d2abddd909f180561
chenjunqian <chenjunqian0810@foxmail.com>
2022-08-16 23:50:15 +0800

    after page switch back to top

--- zimoarts/src/main.js
+++ zimoarts/src/main.js
@@ -5,4 +5,8 @@ import router from './router'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
+router.afterEach((to, from, next) => {
+    window.scrollTo(0, 0);
+})
+
 createApp(App).use(Antd).use(router).mount('#app')
