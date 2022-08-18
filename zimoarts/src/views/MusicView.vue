abd712871bae8b046ef8f8821082c8ad84a9394e
chenjunqian <chenjunqian0810@foxmail.com>
2022-08-18 22:48:27 +0800

    add image list

--- zimoarts/src/views/MusicView.vue
+++ zimoarts/src/views/MusicView.vue
@@ -8,11 +8,11 @@
         <InterviewComponent />
         <ColorDivider />
         <div style="width: 100%;margin-top: 100px;">
-            <HorizontalScrollListView style="box-shadow: 0px 20px 10px -10px #888888;"
-                :images="['https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg','https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg','https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg','https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg','https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg','https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg','https://bbk12e1-cdn.myschoolcdn.com/ftpimages/542/link/large_link1625891_91604.jpg']" />
+            <HorizontalScrollListView style="box-shadow: 0px 20px 10px -10px #888888;" :images="imageList" />
         </div>
-        <InterviewColumnComponent backgroudPicUrl="http://www.grotonarts.com/static/music/music-interview-bg.png" style="margin-top: 80px;"/>
-        <DescriptionColumnComponent style="box-shadow: 0 20px 20px -2px #888888;"/>
+        <InterviewColumnComponent backgroudPicUrl="http://www.grotonarts.com/static/music/music-interview-bg.png"
+            style="margin-top: 80px;" />
+        <DescriptionColumnComponent style="box-shadow: 0 20px 20px -2px #888888;" />
         <FooterComponent style="margin-top: 20px" />
     </div>
 </template>
@@ -27,6 +27,7 @@
     import HorizontalScrollListView from "@/components/HorizontalScrollListView.vue";
     import InterviewColumnComponent from "@/components/InterviewColumnComponent.vue";
     import DescriptionColumnComponent from "@/components/DescriptionColumnComponent.vue";
+    import { httpRequest } from "@/libs/request";
 
     export default {
         name: "MusicView",
@@ -41,6 +42,28 @@
             InterviewColumnComponent,
             DescriptionColumnComponent
         },
+        data() {
+            return {
+                imageList: []
+            }
+        },
+        mounted() {
+            let tempImageList = this.imageList
+            httpRequest.get("http://www.grotonarts.com/static/music/music-image-list.json").then(function (response) {
+                let imageSourcePath = "http://www.grotonarts.com/static/music/image-list/"
+                let allImageList = response["image_list"]
+                for (var key in allImageList) {
+                    let itemImageList = allImageList[key]
+                    for (var i in itemImageList) {
+                        let imageItemPath = imageSourcePath + key + "/" + itemImageList[i]
+                        tempImageList.push(imageItemPath)
+                    }
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+            this.imageList = tempImageList
+        }
     }
 </script>
 
