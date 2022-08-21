cf81ad1429e189f48eb309a9d4a9dd37a19d8988
chenjunqian <chenjunqian0810@foxmail.com>
2022-08-21 21:47:37 +0800

    format

--- zimoarts/src/components/PageTopBigImageComponent.vue
+++ zimoarts/src/components/PageTopBigImageComponent.vue
@@ -1,40 +1,40 @@
 <template>
-    <div style="width: 100%; margin-top: 30px; background-color: #000000;box-shadow: 0 20px 20px -2px #888888;">
-        <div style="width: 100%; margin-top: 30px; background-color: #000000;background-size: 100% auto;background-repeat: no-repeat;height: 1065px;position: relative;"
-            :style="{'background-image': `url('${backgroudPicUrl}')`}">
-            <!-- text block -->
-            <div style="padding-left: 100px; padding-top: 120px;">
-                <!-- visual art title -->
-                <div style="width: 100%; display: flex;">
-                    <div style="font-size: 70px;" :style="{'color': titleColor }">{{titleOne}}</div>
-                    <div style="font-size: 70px;font-weight: bolder;" :style="{'color': titleColor }">{{titleTwo}}</div>
-                </div>
-            </div>
-
-            <div
-                style="width: 600px;height: 337.5px;background-color: #c0c0c0;margin-left: 100px;position: absolute;bottom: -80px;">
-                <VideoPlayerComponent vWidth="600px" vHeight="337.5px" :vSrc="videoUrl" />
-            </div>
+  <div style="width: 100%; margin-top: 30px; background-color: #000000;box-shadow: 0 20px 20px -2px #888888;">
+    <div
+      style="width: 100%; margin-top: 30px; background-color: #000000;background-size: 100% auto;background-repeat: no-repeat;height: 1065px;position: relative;"
+      :style="{ 'background-image': `url('${backgroudPicUrl}')` }">
+      <!-- text block -->
+      <div style="padding-left: 100px; padding-top: 120px;">
+        <!-- visual art title -->
+        <div style="width: 100%; display: flex;">
+          <div style="font-size: 70px;" :style="{ 'color': titleColor }">{{ titleOne }}</div>
+          <div style="font-size: 70px;font-weight: bolder;" :style="{ 'color': titleColor }">{{ titleTwo }}</div>
         </div>
-        <div style="width: 100%; height: 80px;background-color: #ffffff;"></div>
+      </div>
+
+      <div
+        style="width: 600px;height: 337.5px;background-color: #c0c0c0;margin-left: 100px;position: absolute;bottom: -80px;">
+        <VideoPlayerComponent vWidth="600px" vHeight="337.5px" :vSrc="videoUrl" />
+      </div>
     </div>
+    <div style="width: 100%; height: 80px;background-color: #ffffff;"></div>
+  </div>
 </template>
 
 <script>
-    import VideoPlayerComponent from './VideoPlayerComponent.vue'
-    export default {
-        name: "PageTopBigImageComponent",
-        props: {
-            titleOne: String,
-            titleTwo: String,
-            titleColor: String,
-            backgroudPicUrl: String,
-            videoUrl: String
-        },
-        components: { VideoPlayerComponent, VideoPlayerComponent }
-    }
+  import VideoPlayerComponent from './VideoPlayerComponent.vue'
+  export default {
+    name: "PageTopBigImageComponent",
+    props: {
+      titleOne: String,
+      titleTwo: String,
+      titleColor: String,
+      backgroudPicUrl: String,
+      videoUrl: String
+    },
+    components: { VideoPlayerComponent, VideoPlayerComponent }
+  }
 </script>
 
 <style>
-
 </style>
