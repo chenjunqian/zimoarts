84ff68895d01897b131de7d9b465734f2cb23332
chenjunqian <chenjunqian0810@foxmail.com>
2022-08-31 23:25:10 +0800

    add footer resource

--- zimoarts/src/components/FootComponent.vue
+++ zimoarts/src/components/FootComponent.vue
@@ -11,22 +11,17 @@
         </div>
       </div>
       <div class="center-footer-container">
-        <img
-          class="center-footer-logo"
-          src="@/assets/school-bagde.png"
-          alt=""
-        />
+        <img class="center-footer-logo" src="@/assets/school-bagde.png" alt="" />
       </div>
       <div class="right-footer-container">
         <div class="right-footer-logo-container">
           <img class="right-footer-logo" src="@/assets/black-school-name.png">
         </div>
         <div class="right-footer-text-column">
-          <div class="right-footer-text">JGYTDETRTQWEESDFG</div>
-          <div class="right-footer-text">GDGHGHGDFGSDTSIUHIOVBBXZMIYTUXDG</div>
-          <div class="right-footer-text">HJHFJH JHCH V FJ VJVCCCC H</div>
-          <div class="right-footer-text">GJHG JKHVJHG VHVHFV</div>
-          <div class="right-footer-text">CHGFD</div>
+          <div class="right-footer-text">Communication:</div>
+          <div class="right-footer-text">{{ pageData.email }}</div>
+          <div class="right-footer-text">{{ pageData.phone }}</div>
+          <div class="right-footer-text">{{ pageData.name }}</div>
         </div>
       </div>
     </div>
@@ -35,110 +30,132 @@
 </template>
 
 <script>
-export default {
-  name: "FooterComponent",
-};
+  import { httpRequest } from '@/libs/request';
+  import { reactive } from 'vue';
+
+  export default {
+    name: "FooterComponent",
+    setup() {
+      let pageData = reactive({
+        email: "",
+        phone: "",
+        name: ""
+      })
+      httpRequest.get("http://www.grotonarts.com/static/footer/footer-resource.json").then(function (response) {
+        pageData.email = response["email"]
+        pageData.phone = response["phone"]
+        pageData.name = response["name"]
+
+      }).catch(function (error) {
+        console.log(error);
+      });
+
+      return {
+        pageData
+      }
+    }
+  };
 </script>
 
 <style scoped>
-.footer-container {
-  width: 100%;
-  height: 120px;
-  display: flex;
-  flex-wrap: wrap;
-}
+  .footer-container {
+    width: 100%;
+    height: 120px;
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-.footer-container-right-placeholder {
-  width: 5%;
-  height: 20px;
-}
+  .footer-container-right-placeholder {
+    width: 5%;
+    height: 20px;
+  }
 
-.footer-container-left-placeholder {
-  width: 5%;
-  height: 20px;
-}
+  .footer-container-left-placeholder {
+    width: 5%;
+    height: 20px;
+  }
 
-.footer-container-center-wrapper {
-  width: 90%;
-  display: flex;
-  flex-wrap: wrap;
-}
+  .footer-container-center-wrapper {
+    width: 90%;
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-.top-footer-rights {
-  width: 90%;
-}
+  .top-footer-rights {
+    width: 90%;
+  }
 
-/* left block */
-.left-footer-container {
-  width: 30%;
-  justify-content: center;
-}
+  /* left block */
+  .left-footer-container {
+    width: 30%;
+    justify-content: center;
+  }
 
-.left-footer-top-text {
-  width: 100%;
-  height: 30%;
-  margin-top: 15px;
-  display: flex;
-  font-size: 23px;
-  color: brown;
-  font: bolder;
-}
+  .left-footer-top-text {
+    width: 100%;
+    height: 30%;
+    margin-top: 15px;
+    display: flex;
+    font-size: 23px;
+    color: brown;
+    font: bolder;
+  }
 
-.left-footer-text-bottom-container {
-  width: 100%;
-  display: flex;
-  height: 50%;
-  position: relative;
-}
+  .left-footer-text-bottom-container {
+    width: 100%;
+    display: flex;
+    height: 50%;
+    position: relative;
+  }
 
-.left-footer-bottom-text {
-  width: 100%;
-  display: flex;
-  font-size: 8px;
-  position: absolute;
-  bottom: 0;
-}
+  .left-footer-bottom-text {
+    width: 100%;
+    display: flex;
+    font-size: 8pt;
+    position: absolute;
+    bottom: 0;
+  }
 
-/* center block */
-.center-footer-container {
-  width: 40%;
-  justify-content: center;
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-}
+  /* center block */
+  .center-footer-container {
+    width: 40%;
+    justify-content: center;
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-.center-footer-logo {
-  width: 81.25px;
-  height: 100px;
-}
+  .center-footer-logo {
+    width: 81.25px;
+    height: 100px;
+  }
 
-/* right block */
-.right-footer-container {
-  width: 30%;
-  justify-content: center;
-}
+  /* right block */
+  .right-footer-container {
+    width: 30%;
+    justify-content: center;
+  }
 
-.right-footer-logo-container {
-  width: 100%;
-  height: 25px;
-  padding-top: 15px;
-  position: relative;
-}
+  .right-footer-logo-container {
+    width: 100%;
+    height: 25px;
+    padding-top: 15px;
+    position: relative;
+  }
 
-.right-footer-logo {
-  width: 150px;
-  position: absolute;
-  right: -8px;
-}
+  .right-footer-logo {
+    width: 150px;
+    position: absolute;
+    right: -8px;
+  }
 
-.right-footer-text-column {
-  padding-top: 20px;
-}
+  .right-footer-text-column {
+    padding-top: 20px;
+  }
 
-.right-footer-text {
-  width: 100%;
-  font-size: 8px;
-  text-align: right;
-}
+  .right-footer-text {
+    width: 100%;
+    font-size: 8pt;
+    text-align: right;
+  }
 </style>
