af423b8719f2543f8125e3846bf0e79546983519
chenjunqian <chenjunqian0810@foxmail.com>
2022-10-29 01:14:11 +0800

    ignore param

--- zimoarts/src/main.js
+++ zimoarts/src/main.js
@@ -5,8 +5,8 @@ import router from './router'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 
-router.afterEach((to, from, next) => {
-    window.scrollTo(0, 0);
+router.afterEach((_to, _from, _next) => {
+  window.scrollTo(0, 0);
 })
 
 createApp(App).use(Antd).use(router).mount('#app')
